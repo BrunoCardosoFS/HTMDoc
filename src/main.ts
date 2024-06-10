58c9f2b0f648038f43c2d8d1453ae4818e82aee0
Bruno Cardoso <bc13092002@gmail.com>
2024-06-10 20:39:52 -0300

    Layout

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import './style.css'
 import './print.css'
 
-document.querySelector<HTMLElement>('#app')!.innerHTML = `
+document.querySelector<HTMLElement>('#files')!.innerHTML = `
+<span>Files</span>
+`
 
+document.querySelector<HTMLElement>('#editor')!.innerHTML = `
+<span>Editor</span>
 `
 
 document.querySelector<HTMLDivElement>('#render')!.innerHTML = `
-
+<span>Render</span>
 `
