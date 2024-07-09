16e45da2dc017f18f7af7a32da77c09594b5bbb2
Bruno Cardoso <bc13092002@gmail.com>
2024-07-09 15:55:44 -0300

    started adding monaco-editor

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import './style.css'
 import './print.css'
 
+import * as monaco from 'monaco-editor'
+import { emmetHTML } from 'emmet-monaco-es'
+
 document.querySelector<HTMLElement>('#files')!.innerHTML = `
 <span>Files</span>
 `
 
-document.querySelector<HTMLElement>('#editor')!.innerHTML = `
-<span>Editor</span>
-`
+document.querySelector<HTMLElement>('#editor')!.innerHTML = ``
 
 document.querySelector<HTMLDivElement>('#render')!.innerHTML = `
 <span>Render</span>
 `
+
+monaco.editor.create(document.querySelector<HTMLElement>('#editor')!, {
+    value: '',
+    language: 'html',
+    theme: "vs-dark"
+});
+
+emmetHTML(monaco, ["html", "css"])
