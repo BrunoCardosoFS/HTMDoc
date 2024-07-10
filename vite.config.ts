c7b36cea9e5ec868256ee730c8aa78b46006fb0c
Bruno Cardoso <bc13092002@gmail.com>
2024-07-10 13:58:04 -0300

    Added MonacoEnvironment

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,16 @@ export default defineConfig({
             key: './ssl/localhost-key.key',
             cert: './ssl/localhost-key.crt'
         }
+    },
+    build: {
+        rollupOptions: {
+          output: {
+            manualChunks(id) {
+              if (id.includes('node_modules')) {
+                return 'vendor'; // Todos os módulos de terceiros vão para um chunk chamado 'vendor'
+              }
+            }
+          }
+        }
     }
 });
