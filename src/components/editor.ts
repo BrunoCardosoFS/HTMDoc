19c0c573dbc4d827aebcdd48ea194967f5d1dd05
Bruno Cardoso <bc13092002@gmail.com>
2024-07-30 15:47:51 -0300

    Render style

--- src/components/editor.ts
+++ src/components/editor.ts
@@ -28,13 +28,40 @@ export default function loadEditor(selector: string){
     var editor = monaco.editor.create(document.querySelector<HTMLElement>(selector)!, {
         value: '',
         language: 'html',
-        theme: "vs-dark"
+        theme: "vs-dark",
+        wordWrap: "on"
     });
     emmetHTML(monaco, ["html", "css"])
     
     document.getElementById("renderContent")?.addEventListener("click", function(){
-        console.log(editor.getValue())
         document.querySelector<HTMLDivElement>('#render')!.innerHTML = editor.getValue();
+
+        // var valueEditor = editor.getValue()
+        // console.log(valueEditor)
+
+        // fetch("http://localhost:3000/data", {
+        //     method: "POST",
+        //     body: JSON.stringify({
+        //         html: valueEditor
+        //     }),
+        //     headers: {
+        //         "Content-type": "application/json; charset=UTF-8"
+        //     }
+        // })
+        // .then((response) => response.blob())
+        // .then((blob) => {
+        //     const url = window.URL.createObjectURL(blob);  // Cria uma URL para o Blob
+        //     const a = document.createElement("a");  // Cria um elemento <a>
+        //     a.style.display = "none";
+        //     a.href = url;
+        //     a.download = "response.pdf";  // Nome do arquivo para download
+        //     document.body.appendChild(a);
+        //     a.click();  // Força o clique no link para download
+        //     window.URL.revokeObjectURL(url);  // Libera a URL do Blob
+        // })
+        // .catch((error) => {
+        //     console.error(error)
+        // })
     })
 
 }
